c174507aceb04f240e58e727ce1dc369ea5557f8
leo-berte <leonardo.bertelli96@gmail.com>
2025-10-08 18:00:49 +0200

    updated docker container bash file

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-xhost +local:root
+xhost +local:docker
 
 docker run -it --rm \
-  --env="DISPLAY=$DISPLAY" \
-  --env="QT_X11_NO_MITSHM=1" \
-  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --device=/dev/video1:/dev/video0 \
-  --device=/dev/video6:/dev/video1 \
-  --device=/dev/video8:/dev/video2 \
+  --env DISPLAY=$DISPLAY \
+  --env XAUTHORITY=$XAUTHORITY \
+  --env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+  --env QT_X11_NO_MITSHM=1 \
+  --env ROS_DOMAIN_ID=39 \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  --net host \
+  --ipc host \
+  --pid host \
+  --device /dev/video10:/dev/video0 \
+  --device /dev/video6:/dev/video1 \
+  --device /dev/video8:/dev/video2 \
   webcam_ros2 \
